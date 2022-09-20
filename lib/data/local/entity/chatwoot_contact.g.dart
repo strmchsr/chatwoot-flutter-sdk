a88d0db30f1c6c360b81c567e1d954a839943d68
Shashank Srivastava <shashank090789@gmail.com>
2022-09-20 16:07:03 +0530

    optional email parameter

--- lib/data/local/entity/chatwoot_contact.g.dart
+++ lib/data/local/entity/chatwoot_contact.g.dart
@@ -21,7 +21,7 @@ class ChatwootContactAdapter extends TypeAdapter<ChatwootContact> {
       contactIdentifier: fields[1] as String?,
       pubsubToken: fields[2] as String?,
       name: fields[3] as String,
-      email: fields[4] as String,
+      email: fields[4] as String?,
     );
   }
 
@@ -62,7 +62,7 @@ ChatwootContact _$ChatwootContactFromJson(Map<String, dynamic> json) {
     contactIdentifier: json['source_id'] as String?,
     pubsubToken: json['pubsub_token'] as String?,
     name: json['name'] as String,
-    email: json['email'] as String,
+    email: json['email'] as String?,
   );
 }
 
